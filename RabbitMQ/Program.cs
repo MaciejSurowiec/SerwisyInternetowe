fabbe709cad976f625b7e94cbde1d714653fd33c
Kamil Suchecki <Kamil1mld@gmail.com>
2022-11-15 12:27:06 +0100

    Updated with dockercompose

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(System.IO.File.Open("config.txt", FileMode.Open));
+            string username = sr.ReadLine();
+            string password = sr.ReadLine();
+            string host = sr.ReadLine();
+            string port = sr.ReadLine();
+            string endpoint = sr.ReadLine();
+            sr.Close();
+            var rng = new Random();
+
+            var factory = new Client.ConnectionFactory()
+            {
+                UserName = username,
+                HostName = host,
+                Password = password,
+                VirtualHost = ConnectionFactory.DefaultVHost,
+                Port = int.Parse(port)
+            };
+
+            Guid deviceguid = new Guid();
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(endpoint, true, false, false, null);
+                    channel.BasicQos(0, 1, true);
+                    while (true)
+                    {
+                        var properties = channel.CreateBasicProperties();
+                        properties.Headers = new Dictionary<string, object>();
+
+                        var msg = new TempICis
+                        {
+                            Temp = rng.Next(40),
+                            cisnienie = 800 + rng.Next(400),
+                            timestamp = DateTime.Now.ToBinary(),
+                            guid = deviceguid
+                        };
+
+                        properties.Persistent = true;
+                        ReadOnlyMemory<byte> body = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(msg));
+                        channel.BasicPublish("", endpoint, false, properties, body);
+
+                        System.Threading.Thread.Sleep(5000);
+                    }
+                }
+            }
+        }
+    }
+    public interface ITemperatura
+    {
+        int Temp { get; set; }
+    }
+
+    public interface ITimestamp
+    {
+        long timestamp { get; set; }
+    }
+
+    public interface IGuid
+    {
+        Guid guid { get; set; }
+    }
+    public interface ICisnienie
+    {
+        int cisnienie { get; set; }
+    }
+
+    public class TempICis : ICisnienie, ITemperatura, ITimestamp, IGuid
+    {
+        public int Temp { get; set; }
+        public int cisnienie { get; set; }
+        public long timestamp { get; set; }
+        public Guid guid { get; set; }
+    }
+}
---
+++ SerwisyInternetowe/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.IO;
+
+namespace SerwisyInternetowe
+{
+
+    public class Program
+    {
+        public static string username {get; set;}
+        public static string password {get; set;}
+        public static string host {get; set;}
+        public static string vhost {get; set;}
+        public static string endpoint {get; set;}
+
+
+        public static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(System.IO.File.Open("config.txt", System.IO.FileMode.Open)); 
+            username = sr.ReadLine();
+            password = sr.ReadLine();
+            host = sr.ReadLine();
+            vhost = ConnectionFactory.DefaultVHost;
+            endpoint = sr.ReadLine();
+
+
+            CreateHostBuilder(args).Build().Run();
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
